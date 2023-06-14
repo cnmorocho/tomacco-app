582ced1613c2bcea5380c4ba0fd9054914d6ecaf
cnmorocho <nahuelmorocho@gmail.com>
2023-06-13 22:34:33 -0300

    navbar added in home

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,9 +1,11 @@
 import DesktopWrapper from '@/components/DesktopWrapper';
+import Navbar from '@/components/Navbar';
 import TimerSection from '@/components/TimerSection.tsx';
 
 export default function Home() {
     return (
         <DesktopWrapper title='Tomacco'>
+            <Navbar />
             <TimerSection />
         </DesktopWrapper>
     );
