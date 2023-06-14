8c8ac032700311e0069fc5c85fcb4b419da9ae5a
cnmorocho <nahuelmorocho@gmail.com>
2023-06-13 22:42:48 -0300

    info page created

---
+++ pages/info/index.tsx
@@ -0,0 +1,11 @@
+import DesktopWrapper from '@/components/DesktopWrapper';
+
+const UsageInformation = () => {
+    return (
+        <DesktopWrapper title='Tomacco | Informacion de uso'>
+            <div></div>
+        </DesktopWrapper>
+    );
+};
+
+export default UsageInformation;
