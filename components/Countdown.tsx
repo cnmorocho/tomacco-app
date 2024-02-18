c6405076604b73ee8216eafc5d1467c7d0fde63f
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2024-02-18 19:53:37 -0300

    fix: change the confirmation alert to modal

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { robotoSerif, roboto } from '@/fonts';
 import { useAppDispatch } from '@/redux/hooks';
 import { reset } from '@/redux/slices/countdown';
+import ConfirmationModal from './ConfirmationModal';
 
 type CountdownProps = {
     minutes: string;
@@ -10,23 +11,19 @@ type CountdownProps = {
 };
 
 const Countdown = ({ minutes, seconds, currentInterval }: CountdownProps) => {
-    const dispatch = useAppDispatch();
-
-    const resetCountdown = (): void => {
-        if (confirm('¿Esta seguro que desea reiniciar el pomodoro?'))
-          dispatch(reset());
-    };
+    const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
     return (
         <div className={`${robotoSerif.className} flex flex-col items-center text-zinc-800`}>
             <p
-                onClick={resetCountdown}
-                className={`${roboto.className} text-lg cursor-pointer font-normal p-1 hover:bg-zinc-700 hover:text-zinc-50`} >
+                onClick={() => setShowConfirmationModal(true)}
+                className={`${roboto.className} text-lg cursor-pointer font-normal p-1 hover:bg-zinc-700 hover:text-zinc-50 transition duration-200`} >
                 #{currentInterval}
             </p>
             <p className="text-6xl font-bold">
                 {minutes}:{seconds}
             </p>
+            <ConfirmationModal title='Reset countdown' description='Are you sure you want to reset the countdown and set the interval to zero?' isVisible={showConfirmationModal} toggleVisiblity={setShowConfirmationModal} />
         </div>
     );
 };
