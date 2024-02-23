0e4599b3db461b81a27b0014a11762b66c4b6f8a
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2024-02-22 22:27:41 -0300

    refactor: create Quote component

---
+++ src/components/Quote.tsx
@@ -0,0 +1,24 @@
+import { robotoSerif } from '@/fonts';
+import { getRandomMotivationalQuote } from '@/services/quotes';
+import { useEffect, useState } from 'react';
+
+export default function Quote() {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    getRandomMotivationalQuote().then(setQuote);
+  }, []);
+
+  return (
+    <div
+      className={`${robotoSerif.className} flex justify-center text-zinc-700 font-normal`}>
+      {quote ? (
+        <p className='w-auto py-1 px-1 text-center hover:bg-zinc-700 hover:text-zinc-50 hover:cursor-pointer transition duration-200 text-md italic'>
+          "{quote}"
+        </p>
+      ) : (
+        <div className='animate-pulse h-5 bg-zinc-500 rounded-full w-36'></div>
+      )}
+    </div>
+  );
+}
