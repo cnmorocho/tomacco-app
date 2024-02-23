ad07ad62658dd3bd0902a57ea8d607259f28646a
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2024-02-22 22:28:42 -0300

    refactor: title deleted

--- src/components/DesktopWrapper.tsx
+++ src/components/DesktopWrapper.tsx
@@ -1,16 +1,10 @@
-import Head from 'next/head';
-
 type DesktopWrapperProps = {
     children: JSX.Element | JSX.Element[];
-    title: string;
 };
 
-const DesktopWrapper = ({ children, title }: DesktopWrapperProps) => {
+const DesktopWrapper = ({ children }: DesktopWrapperProps) => {
     return (
         <div className="m-0 w-full h-screen p-0 bg-zinc-50">
-            <Head>
-                <title>{title}</title>
-            </Head>
             {children}
         </div>
     );
