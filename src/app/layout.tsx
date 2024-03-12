10d46da8a388360fd1712dbdf99edb7a06eddd0e
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2024-03-11 21:06:39 -0300

    Added the type of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Tomacco',
   description: 'Tomacco App',
 };
