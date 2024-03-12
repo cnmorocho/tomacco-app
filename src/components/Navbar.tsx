833ba52f3f24d6abdb3997837d70ff6676307cb3
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2024-03-11 21:00:18 -0300

    Added MUI icons to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,43 @@ import { getWeather } from '@/services/weather';
 import NameModal from './NameModal';
 import { useAppSelector } from '@/redux/hooks';
 import LocationModal from './LocationModal';
+import CloudCircleIcon from '@mui/icons-material/CloudCircle';
 
 const Navbar = () => {
-  const [weather, setWeather] = useState('');
-  const [showNameModal, setShowNameModal] = useState(false);
-  const [showSetLocationModal, setShowLocationModal] = useState(false);
+  return (
+    <div
+      className={`${roboto.className} w-full h-7 flex flex-row justify-around items-center font-normal text-sm border-zinc-300 border-b-[1.8px]`}>
+      <NavName />
+      <div className='flex flex-row gap-1'>
+        <WeatherNavItem />
+      </div>
+    </div>
+  );
+};
+
+function NavName() {
   const userName = useAppSelector(state => state.user.name);
+  const [showNameModal, setShowNameModal] = useState(false);
+
+  return (
+    <div>
+      <p
+        className='hover:border-zinc-300 hover:cursor-pointer transition duration-200 flex items-center h-full px-2 border-x border-zinc-50'
+        onClick={() => setShowNameModal(true)}>
+        {userName}'s Tomacco
+      </p>
+      <NameModal
+        title='Set name'
+        isVisible={showNameModal}
+        toggleVisiblity={setShowNameModal}
+      />
+    </div>
+  );
+}
+
+function WeatherNavItem() {
+  const [weather, setWeather] = useState('');
+  
   const location = useAppSelector(state => state.location);
 
   useEffect(() => {
@@ -17,32 +48,23 @@ const Navbar = () => {
       setWeather(currentWeather.current['temperature_2m']),
     );
   }, [location]);
-
-
+  const [showSetLocationModal, setShowLocationModal] = useState(false);
   return (
-    <div
-      className={`${roboto.className} w-full h-auto flex flex-row justify-around py-2 font-normal text-sm border-zinc-300 border-b-[1.8px]`}>
-      <p
-        className='font-medium hover:bg-zinc-700 hover:text-zinc-50 hover:cursor-pointer transition duration-200'
-        onClick={() => setShowNameModal(true)}>
-        {userName}'s Tomacco
-      </p>
-      <div className='flex flex-row items-center w-36 max-w-44'>
+    <div>
+      <div className='flex flex-row items-center h-full'>
         {location.name && weather ? (
-          <p
-            className='hover:bg-zinc-700 hover:text-zinc-50 hover:cursor-pointer transition duration-200'
+          <div
+            className='border-x border-zinc-50 hover:border-zinc-300 hover:cursor-pointer transition duration-200 flex items-center h-full px-2 box-border gap-1'
             onClick={() => setShowLocationModal(true)}>
-            {location.name}, {weather}° C
-          </p>
+            <CloudCircleIcon fontSize='inherit' sx={{ fontSize: 13 }} />
+            <p>
+              {location.name}, {weather}° C
+            </p>
+          </div>
         ) : (
           <div className='animate-pulse h-3 bg-zinc-500 rounded-full w-36 transition duration-500'></div>
         )}
       </div>
-      <NameModal
-        title='Set name'
-        isVisible={showNameModal}
-        toggleVisiblity={setShowNameModal}
-      />
       <LocationModal
         title='Set location'
         isVisible={showSetLocationModal}
@@ -50,6 +72,6 @@ const Navbar = () => {
       />
     </div>
   );
-};
+}
 
 export default Navbar;
