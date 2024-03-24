4c74a9050e16833fc1b30a07937392dc74f91266
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2024-03-24 17:30:09 -0300

    chore: se elimino la posibilidad de un posible error de ruteo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/_error',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
